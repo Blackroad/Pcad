1807b8b23faf794d59c7525f3a1a58d41cb50a72
Blackroad99 <Vladvell@gmail.com>
2018-09-27 02:20:50 +1400

    cleared Dashboard and cases to PEP8

--- fixture/db_core.py
+++ fixture/db_core.py
@@ -1,10 +1,9 @@
 from db import DB
 
+
 class DataBaseHelper:
     def __init__(self):
-        self.db = DB(username='webdeploy', password='123Abc+-=', hostname='phxm1bappd34\\tsop',dbtype='mssql')
-
-
+        self.db = DB(username='webdeploy', password='123Abc+-=', hostname='phxm1bappd34\\tsop', dbtype='mssql')
 
 # if __name__ == "__main__":
 #     query = 'SELECT TOP (1000) [UserId] '\
@@ -12,4 +11,3 @@ class DataBaseHelper:
 #   'FROM [LTC].[dbo].[User]'
 #     datab = DataBaseHelper()
 #     print (datab.db.query(query))
-
